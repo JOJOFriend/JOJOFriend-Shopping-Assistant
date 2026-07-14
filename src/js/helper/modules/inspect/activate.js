ff04ca614c893dc1e323c6dbf5a8ced8b0839da0
JOJOFriend <extension@jojofriend.com>
2026-07-14 18:02:00 +0800

    Optimize the method of adding element CSS.

--- src/js/helper/modules/inspect/activate.js
+++ src/js/helper/modules/inspect/activate.js
@@ -6,9 +6,14 @@
      */
     $.ActivateHelper = function (ext) {
         const MIN_ACTIVATE_WIDGET_TOP = 50;
-        const toCssString = (obj) => Object.entries(obj || {})
-            .map(([k, v]) => `${k.replace("_", "-")}:${v}`)
-            .join(";");
+        const toCssObject = (obj={}) =>{
+            const result = {};
+            for (const [key, value] of Object.entries(obj)) {
+                const camelKey = key.replace(/_([a-z])/g, (_, c) => c.toUpperCase());
+                result[camelKey] = value;
+            }
+            return result;
+        };
 
         const getActivateTop = () => {
             const defaultTop = ext.helper.dao.getDefaults().p.logoTop;
@@ -54,7 +59,7 @@
             // Drag handle (right-side grip for vertical dragging)
             const dragHandle = ext.helper.elementUtil.createElement("div", {
                 className: "activate-widget__drag-handle all-center",
-                attributes: { style: toCssString(dragData) },
+                style: toCssObject(dragData),
                 children: [ext.helper.elementUtil.createElement("img", {
                     attributes: { src: "data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='10'%20height='17'%20viewBox='0%200%2010%2017'%3e%3cg%20id='drag_icon'%20data-name='drag%20icon'%20transform='translate(-756.458%20-5682.563)'%3e%3ccircle%20id='Ellipse_277'%20data-name='Ellipse%20277'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(756.458%205682.563)'%20fill='%23fff'/%3e%3ccircle%20id='Ellipse_280'%20data-name='Ellipse%20280'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(763.458%205682.563)'%20fill='%23fff'/%3e%3ccircle%20id='Ellipse_281'%20data-name='Ellipse%20281'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(756.458%205689.563)'%20fill='%23fff'/%3e%3ccircle%20id='Ellipse_283'%20data-name='Ellipse%20283'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(756.458%205696.563)'%20fill='%23fff'/%3e%3ccircle%20id='Ellipse_282'%20data-name='Ellipse%20282'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(763.458%205689.563)'%20fill='%23fff'/%3e%3ccircle%20id='Ellipse_284'%20data-name='Ellipse%20284'%20cx='1.5'%20cy='1.5'%20r='1.5'%20transform='translate(763.458%205696.563)'%20fill='%23fff'/%3e%3c/g%3e%3c/svg%3e", draggable: false }
                 })]
@@ -65,13 +70,13 @@
                 ? [ext.helper.elementUtil.createElement("div", {
                     className: "notification all-center pulse-reveal",
                     text: couponTotal,
-                    attributes: { style: toCssString(badgeData) }
+                    style: toCssObject(badgeData)
                 })]
                 : [];
             const trigger = ext.helper.elementUtil.createElement("div", {
                 className: "activate-widget__trigger",
                 children: triggerChildren,
-                attributes: { style: toCssString(interfaceData) }
+                style: toCssObject(interfaceData)
             });
             const content = ext.helper.elementUtil.createElement("div", {
                 className: "activate-widget__content",
